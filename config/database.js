ecf4f49a57d32e4230d525b9066bb3eb7602642a
BrianCottrell <Brian_Cottrell@inbox.com>
2015-04-09 17:30:25 -0700

    Fixed CSS link error so the site will display properly in firefox.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 //Link the site to modulus database
 module.exports = {
-	url:'mongodb://brian:node@novus.modulusmongo.net:27017/x2uxyQem'
+	url:'mongodb://hackgeny:password@proximus.modulusmongo.net:27017/xizoV8ig'
 };
